c082e9ade4e2bc3b3925c65b249e7cb158f5f371
Sho DOUHASHI <douhashi@gmail.com>
2016-03-12 18:47:59 +0900

    pagination

--- dvd_rental_scraper.go
+++ dvd_rental_scraper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"net/url"
 	"sync"
 )
 
@@ -11,8 +12,8 @@ type ResultItem struct {
 	ReleasedAt string
 }
 
-func GetDVDItem(url string) ResultItem {
-	doc, _ := goquery.NewDocument(url)
+func GetDVDItem(itemUrl string) ResultItem {
+	doc, _ := goquery.NewDocument(itemUrl)
 	title := doc.Find(".header h2 span").First().Text()
 	releasedAt := doc.Find(".detailBox li").First().Next().Text()
 
@@ -34,20 +35,48 @@ func GoGetDVDItems(urls []string) []ResultItem {
 }
 
 // Get dvd item url list
-func GetDVDList(url string) []string {
-	list := []string{}
+func GetDVDItemUrls(url string) []string {
+	urls := []string{}
 	doc, _ := goquery.NewDocument(url)
 	doc.Find(".itemGroup .imageBlock a").Each(func(_ int, s *goquery.Selection) {
 		url, _ := s.Attr("href")
-		list = append(list, url)
+		urls = append(urls, url)
 	})
-	return list
+	return urls
+}
+
+func GetDVDPages(url string) []string {
+	urls := []string{url}
+	doc, _ := goquery.NewDocument(url)
+	doc.Find("ul.pageList").First().Find("li").Each(func(_ int, li *goquery.Selection) {
+		a := li.Find("a").First()
+		aClass, _ := a.Attr("class")
+		liClass, _ := li.Attr("class")
+
+		if liClass != "last" && aClass != "active" {
+			href, _ := a.Attr("href")
+			fullUrl := GenerateUrlWithPath(url, href)
+			urls = append(urls, fullUrl)
+		}
+	})
+	return urls
+}
+
+func GenerateUrlWithPath(pageUrl string, path string) string {
+	u, _ := url.Parse(pageUrl)
+	fullUrl := u.Scheme + "://" + u.Host + path
+	return fullUrl
 }
 
 func main() {
 	url := "http://store-tsutaya.tsite.jp/top/rels/dvd_rental.html"
-	list := GetDVDList(url)
-	results := GoGetDVDItems(list)
+	pages := GetDVDPages(url)
+
+	itemUrls := []string{}
+	for _, page := range pages {
+		itemUrls = append(itemUrls, GetDVDItemUrls(page)...)
+	}
+	results := GoGetDVDItems(itemUrls)
 
 	for _, result := range results {
 		fmt.Println(result.Title + " : " + result.ReleasedAt)
