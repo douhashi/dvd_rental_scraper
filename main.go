9c4960d56b22e5faafeb2f902113ecbc31a9954f
Sho DOUHASHI <douhashi@gmail.com>
2016-03-12 16:46:59 +0900

    title and released at

--- main.go
+++ main.go
@@ -5,15 +5,35 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func GetPage(url string) {
+type ItemResult struct {
+	Title      string
+	ReleasedAt string
+}
+
+func GetDVDItem(url string) ItemResult {
 	doc, _ := goquery.NewDocument(url)
-	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+	title := doc.Find(".header h2 span").First().Text()
+	releasedAt := doc.Find(".detailBox li").First().Next().Text()
+
+	result := ItemResult{title, releasedAt}
+	return result
+}
+
+func GetDVDList(url string) []string {
+	list := []string{}
+	doc, _ := goquery.NewDocument(url)
+	doc.Find(".itemGroup .imageBlock a").Each(func(_ int, s *goquery.Selection) {
 		url, _ := s.Attr("href")
-		fmt.Println(url)
+		list = append(list, url)
 	})
+	return list
 }
 
 func main() {
-	url := "http://qiita.com/advent-calendar/2013/"
-	GetPage(url)
+	url := "http://store-tsutaya.tsite.jp/top/rels/dvd_rental.html"
+	list := GetDVDList(url)
+	for _, v := range list {
+		result := GetDVDItem(v)
+		fmt.Println(result.Title + " : " + result.ReleasedAt)
+	}
 }
